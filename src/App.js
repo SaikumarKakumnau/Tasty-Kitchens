78390ad4163bcc5a85e7165c26038c4f973d6fee
SaikumarKakumanu <106460943+SaikumarKakumnau@users.noreply.github.com>
2023-07-02 12:47:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import {Route, Switch} from 'react-router-dom'
-import LoginForm from './components/LoginForm'
+import {Component} from 'react'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import Login from './components/Login'
 import Home from './components/Home'
+import Cart from './components/Cart'
+import Header from './components/Header'
+import RestaurantDetails from './components/RestaurantDetails'
+import Footer from './components/Footer'
+import ProtectedRoute from './components/ProtectedRoute'
+import NotFound from './components/NotFound'
+import AppTheme from './context/AppTheme'
 
 import './App.css'
 
-const sortByOptions = [
-  {
-    id: 0,
-    displayText: 'Highest',
-    value: 'Highest',
-  },
-  {
-    id: 2,
-    displayText: 'Lowest',
-    value: 'Lowest',
-  },
-]
+class App extends Component {
+  state = {activeTheme: 'light'}
 
-console.log(sortByOptions)
+  changeTheme = activeTheme => {
+    this.setState({activeTheme})
+  }
 
-const App = () => (
-  <div className="app-container">
-    <Switch>
-      <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/" component={Home} />
-    </Switch>
-  </div>
-)
+  render() {
+    const {activeTheme} = this.state
+
+    return (
+      <AppTheme.Provider
+        value={{
+          activeTheme,
+          changeTheme: this.changeTheme,
+        }}
+      >
+        <>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <>
+              <Header />
+              <>
+                <Switch>
+                  <ProtectedRoute exact path="/" component={Home} />
+                  <ProtectedRoute exact path="/cart" component={Cart} />
+                  <ProtectedRoute
+                    exact
+                    path="/restaurant/:id"
+                    component={RestaurantDetails}
+                  />
+                  <Route path="/not-found" component={NotFound} />
+                  <Redirect to="/not-found" />
+                </Switch>
+              </>
+              <Footer />
+            </>
+          </Switch>
+        </>
+      </AppTheme.Provider>
+    )
+  }
+}
 
 export default App
